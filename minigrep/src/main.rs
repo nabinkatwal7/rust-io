c005bdf3545c608a168628a9b1fe5169ce9421e7
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-12 14:12:45 +0545

    add structs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,13 +3,31 @@ use std::fs;
 
 fn main() {
     let args:Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let config = Config::new(&args);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text: \n {contents}");
 }
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config{
+    fn new(args:&[String])->Config{
+        if args.len() < 3 {
+            panic!("Not enough arguments!");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config{query, file_path}
+    }
+}
+
